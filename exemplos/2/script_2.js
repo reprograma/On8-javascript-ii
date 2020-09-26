0cfb8d7003039692890b5821551ff2a16433fd3d
Bruna Roberta Gil <brunagil.eppen@gmail.com>
2020-09-26 16:49:19 -0300

    atualização dos exercicios em sala de aula

--- exemplos/2/script_2.js
+++ exemplos/2/script_2.js
@@ -6,4 +6,31 @@ Hora de interagir com o DOM! Crie um array com no mínimo 5 nomes de cachorros (
 2. Mostre os nomes dos cachorros.
 
 
+PASSO A PASSO
+a. Criar uma array - OK 
+b. Pegar o length da minha array - OK
+c. Colocar no html o length da minha array no texto 
+d. Colocar no html os nomes dos cachorros que estão na minha array
+
+
 */
+
+const cachorros = ["Dakota", "Arizona", "Gloria", "Rex", "Bolinha"]
+const tamanhoArray = cachorros.length
+const resultado = document.getElementById("result");
+const listaDeCachorros = document.getElementById("resultList")
+
+resultado.textContent = `o array de cachorros tem ${tamanhoArray} itens`;
+
+    for(let i=0; i < cachorros.length; i++) {
+        listaDeCachorros.innerHTML += `<li>${cachorros[i]}</li>`
+    }
+
+
+// +=
+// operador que faz com que cada vez que um item passe no loop ele seja incrementado
+
+// textContent = Conteudo de texto => "string"
+// innerHTML = Insere HTML => <h1></h2>
+
+
