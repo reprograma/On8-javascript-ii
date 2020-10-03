e32a913c5597c236b095a61354959fe2c8ad7403
Thaysa <thaysacgms@gmail.com>
2020-10-02 22:59:53 -0300

    exercício - pedra, papel, tesoura

--- exemplos/2/script_2.js
+++ exemplos/2/script_2.js
@@ -7,3 +7,36 @@ Hora de interagir com o DOM! Crie um array com no mínimo 5 nomes de cachorros (
 
 
 */
+
+
+
+let cachorros = ["Dakota", "Arizona", "Gloria", "Rex", "Bolinha"]
+let tamanhoCachorros = cachorros.length;
+let resultado = document.querySelector('.result')
+
+resultado.textContent = `o array tem ${tamanhoCachorros} bichinhos`;
+
+for (let i = 0; i < cachorros.length; i++) {
+        let nomes = document.createElement('li');
+        let nomeDoguinho = document.createTextNode(cachorros[i]);
+        nomes.appendChild(nomeDoguinho);
+        console.log(nomes)
+        
+    const listaDogs = document.getElementById("resultList")
+    listaDogs.appendChild(nomes);
+}
+
+
+
+
+
+
+
+
+// let feedback = document.createElement('p');
+// let messageFeedback = document.createTextNode(`Olá, seu e-mail ${email} foi cadastrado com sucesso`);
+// feedback.appendChild(messageFeedback);
+// input.value = '';
+
+// let messageContainer = document.querySelector('.mensagem');
+// messageContainer.appendChild(feedback);
